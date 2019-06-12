296e21b21af8d6917b024f7048833bc3218e41c9
Ruben Dole <ruben.dole@esme.fr>
2019-06-12 09:07:21 +0000

    Ajout main.tf

---
+++ main.tf
@@ -0,0 +1,27 @@
+provider "scaleway" {
+  region = "${var.region}"
+  organization = "${var.organization}"
+  token        = "${var.scw_token}"
+}
+
+data "scaleway_image" "terraformruben" {
+  architecture = "x86_64"
+  name         = "Ubuntu Bionic"
+}
+
+resource "scaleway_server" "terraformruben" {
+  name  = "terraformruben"
+  image = "${data.scaleway_image.terraformruben.id}"
+  type  = "START1-S"
+}
+resource "scaleway_ip" "ip" {
+  server = "${scaleway_server.terraformruben.id}"
+}
+
+
+resource "scaleway_volume" "terraformruben" {
+  name       = "terraformrubenvolume"
+  size_in_gb = 50
+  type       = "l_ssd"
+}
+
